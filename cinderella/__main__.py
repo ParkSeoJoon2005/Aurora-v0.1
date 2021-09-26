7b4dd83daf18401f45d00074f20a2005ded4b9f1
Park Seo Joon <87398748+parkseojoon2005@users.noreply.github.com>
2021-09-26 08:20:42 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -281,8 +281,8 @@ def get_help(bot: Bot, update: Update):
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="⚜️Help",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="
-                                                                      Dveloper",url="https://t.me/Surv_ivor")]]))
+                                                [InlineKeyboardButton(text="Dveloper",url="https://t.me/Surv_ivor")]]))
+                                                                      
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
